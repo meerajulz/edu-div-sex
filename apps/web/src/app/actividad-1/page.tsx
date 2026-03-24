fcb96f653ab507bc3e493b3393e675e0d2e67a9a
Juliana Leon <juliana.deleonazevedo@eesc.europa.eu>
2026-03-24 17:38:13 +0100

    vercel issues

--- apps/web/src/app/actividad-1/page.tsx
+++ apps/web/src/app/actividad-1/page.tsx
@@ -9,7 +9,7 @@ import {
   cleanupAudio
 } from '../utils/audioPlayer';
 import dynamic from 'next/dynamic';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import JugarButton from '../components/JugarButton/JugarButton';
@@ -382,11 +382,11 @@ useEffect(() => {
     setCanPlayVideo(true);
   };
 
-  const exitVariants = {
+  const exitVariants: Variants = {
     exit: {
       y: '120vh',
       opacity: 0,
-      transition: { duration: 1.5, ease: 'easeInOut' }
+      transition: { duration: 1.5, ease: 'easeInOut' as const }
     }
   };
 
